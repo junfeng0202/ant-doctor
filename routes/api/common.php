aae01bf0851fc6b8b2295ed2e9e90508abc4e986
junfeng <1628439099@qq.com>
2019-02-16 17:00:39 +0800

    wechat getopenid 1.2.1

--- routes/api/common.php
+++ routes/api/common.php
@@ -1,8 +1,8 @@
 <?php
 Route::group(['namespace'=>'App\Http\Controllers\Common'], function (){
 	Route::any('/wechat', 'WeChatController@serve');
-	Route::get('wechat/getCode', 'LoginController@getCode');
-	Route::post('wechat/user', 'LoginController@user');
+	Route::get('wechat/getCode', 'WeChatController@getCode');
+	Route::post('wechat/user', 'WeChatController@user');
 });
 
 Route::group(['namespace'=>'App\Http\Controllers\Common','prefix'=>'v1'], function () {
