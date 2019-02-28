394cb33bf9bb1c14904cc6274ce5572755243f34
夏桐 <xiatong@xiatongdeMacBook-Pro.local>
2019-02-28 11:03:03 +0800

    数据库文件1.1

--- database/migrations/2019_02_28_103543_alter_articles_deleted_at.php
+++ database/migrations/2019_02_28_103543_alter_articles_deleted_at.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterPostsDeletedAt extends Migration
+class AlterArticlesDeletedAt extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AlterPostsDeletedAt extends Migration
     public function up()
     {
         Schema::table('articles', function (Blueprint $table) {
-            $table->softDeletes()->comment('软删除标记字段');;
+            $table->softDeletes();
         });
     }
 
--- database/migrations/2019_02_28_104542_alter_lives_deleted_at.php
+++ database/migrations/2019_02_28_104542_alter_lives_deleted_at.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AlterPostsDeletedAt extends Migration
+class AlterLivesDeletedAt extends Migration
 {
     /**
      * Run the migrations.
@@ -14,7 +14,7 @@ class AlterPostsDeletedAt extends Migration
     public function up()
     {
         Schema::table('lives', function (Blueprint $table) {
-            $table->softDeletes()->comment('软删除标记字段');;
+            $table->softDeletes();
 
         });
     }
