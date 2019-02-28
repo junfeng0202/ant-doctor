333a21efa3c24cad67b83895d48c91c23895909a
夏桐 <xiatong@xiatongdeMacBook-Pro.local>
2019-02-28 15:08:19 +0800

    删除banner

--- app/Models/Banner.php
+++ app/Models/Banner.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Banner extends Model
 {
+    use SoftDeletes;
+
     const INDEX = 0;
     protected $dates = ['deleted_at'];
 
